d0e3032d8f49607565906a1285501a2584ce81ab
lartiguebe <lartiguebe@eisti.eu>
2014-04-30 21:12:41 +0200

    Bug sur admin

---
+++ Java/src/Admin.java
@@ -0,0 +1,20 @@
+public class Admin extends Utilisateur {
+
+//Constructeur
+	public Admin(int idUtilisateur, String nom, String prenom) {
+		super(idUtilisateur, nom, prenom);
+	}
+
+//Méthodes
+	public void creerPromotion(){
+		
+	}
+
+	public void definirModule(){
+		
+	}
+	
+	public void modifierModule(){
+	}
+
+}
