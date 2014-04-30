949794495030a590edd363cf7c68919b9d4ae62c
lartiguebe <lartiguebe@eisti.eu>
2014-04-30 21:24:55 +0200

    methode creerUtilisateur

--- Java/src/Admin.java
+++ Java/src/Admin.java
@@ -6,6 +6,18 @@ public class Admin extends Utilisateur {
 	}
 
 //Méthodes
+	public String creerUtilisateur(String nom, String prenom){
+		String login;
+		login = nom.toLowerCase()+prenom.toLowerCase();
+		if (login.length()<=10){
+			return login;
+		}
+		else{
+			return login.substring(0, 10);
+		}
+		
+	}
+	
 	public void creerPromotion(){
 		
 	}
