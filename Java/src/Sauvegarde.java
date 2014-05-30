38ae8cb569426ed8daa78731299e27448392328f
lartiguebe <lartiguebe@eisti.eu>
2014-05-30 11:46:02 +0200

    Javadoc en place

--- Java/src/Sauvegarde.java
+++ Java/src/Sauvegarde.java
@@ -5,9 +5,21 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
+/**
+ * Classe qui permet de sauvegarder ou de lire le modele.obj
+ * @author Benjamin Lartigue
+ * @version 1.0
+ *
+ */
 
 public class Sauvegarde {
+	
+	
+	/**
+	 * Permets de sauvegarder le modèle après avoir mis toutes ses collections à jour
+	 * @param d Modele à sauvegarder
+	 * @throws IOException
+	 */
 	public static void sauvegardeModele(Modele d) throws IOException{
 		File f = new File("modele.obj");
 		try (ObjectOutputStream ob = new ObjectOutputStream(new FileOutputStream(f))){
@@ -18,6 +30,12 @@ public class Sauvegarde {
 		}
 	}
 	
+	/**
+	 * Permets de lire le modèle s'il est créé, ou tout simplement de le créer
+	 * @return Modele créé ou mise a jour du modèle
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 */
 	public static Modele lireModele() throws IOException, ClassNotFoundException{
 		File f = new File("modele.obj");
 		Modele d;
@@ -25,7 +43,7 @@ public class Sauvegarde {
 			d = (Modele)ob.readObject();
 		}
 		catch (IOException | ClassNotFoundException e){
-			d=new Modele();//Créer un nouveau modèle
+			d=new Modele();//Créer un nouveau modèle s'il n'existe pas encore
 		}
 		return d;
 	}
